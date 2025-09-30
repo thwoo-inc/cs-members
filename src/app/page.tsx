ec698d00a59e0b3a52e9e5733a55ed6cfad4ef13
tetsuyanh <tetsuyanh@gmail.com>
2025-09-30 20:20:38 +0900

    fix screen height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,23 +9,23 @@ export default async function Home() {
   const members = loadMembers();
 
   return (
-    <main className="font-sans min-h-screen relative flex flex-col">
-      <header className="absolute top-0 z-10 bg-[#F0F0F0]/80 w-full h-[40px] sm:h-[64px]">
+    <main className="font-sans h-[100dvh] relative flex flex-col">
+      <header className="absolute top-0 z-10 bg-[#F0F0F0]/80 w-full h-[64px] sm:h-[80px]">
         <div className="container p-4 sm:p-8 flex items-center justify-between mx-auto h-full">
-          <h1 className="flex flex-row gap-2 sm:gap-4 font-bold relative items-center">
+          <h1 className="flex flex-row gap-4 sm:gap-8 font-bold relative items-center">
             <Image
               src="./img/logo.png"
               width={468}
               height={225}
               alt="みんなでつくる中国山地 百年会議"
-              className="w-[52px] sm:w-[104px] h-[25px] sm:h-[50px] "
+              className="w-[104px] h-[50px]"
             />
-            <p className="text-sm sm:text-base">会員マップ</p>
+            <p className="text-lg font-semibold">会員マップ</p>
           </h1>
           <HamburgerMenu />
         </div>
       </header>
-      <div className="w-full min-h-screen relative">
+      <div className="w-full h-full relative">
         <MemberGeoGraph members={members} />
       </div>
       <footer className="absolute flex justify-center items-center bottom-0 text-xs sm:text-sm z-10 h-[24px] sm:h-[36px] bg-[#F0F0F0]/80 w-full p-4">
