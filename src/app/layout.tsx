c03df4621bda86d84063c878ec5f3540ff96611a
tetsuyanh <tetsuyanh@gmail.com>
2025-09-30 20:58:02 +0900

    make bg light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,16 +27,12 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}>
-        {/* ブラー背景画像 */}
         <div
-          className="fixed inset-0 bg-cover bg-center bg-no-repeat bg-fixed blur-[3px] -z-20"
+          className="fixed inset-0 bg-cover bg-center bg-no-repeat bg-fixed -z-20"
           style={{
             backgroundImage: "url('./img/bg.jpg')",
           }}
         />
-
-        {/* 半透明オーバーレイ */}
-        <div className="fixed inset-0 bg-white/50 -z-10" />
 
         <AppProvider>{children}</AppProvider>
       </body>
